a17fe8f025c37383c12d98a52d2c8370df0ee6d6
Ilona <ilonarus@hotmail.com>
2024-01-31 15:32:08 +0100

    First commit

---
+++ src/components/Question.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+import Answers from "./Answers"
+import QuestionTimer from "./QuestionTimer"
+import QUESTIONS from '../questions'
+
+export default function Question({
+  index,
+  onSelectAnswer,
+  onSkipAnswer }) {
+
+    const [answer, setAnswer] = useState({
+      selectedAnswer: '',
+      isCorrect: null
+    })
+
+    let timer = 10000
+
+    if (answer.selectedAnswer) {
+      timer = 1000
+    }
+
+    if (answer.isCorrect !== null) {
+      timer = 2000
+    }
+
+    function handleSelectAnswer(answer) {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer
+      }) 
+
+      setTimeout(() => {
+        setAnswer({
+          selectedAnswer: answer,
+          isCorrect: true
+        }) 
+
+        setTimeout(() => {
+          onSelectAnswer(answer)
+        }, 2000)
+      }, 1000)
+    }
+
+    let answerState = ''
+
+    if (answer.selectedAnswer) {
+      answerState = answer.isCorrect ? 'correct' : 'wrong'
+    }
+
+  return <div id="question">
+    <QuestionTimer
+    key={timer}
+      timeout={timer}
+      onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+      mode={answerState}
+    />
+    <h2>{QUESTIONS[index].text}</h2>
+    <Answers
+      answers={QUESTIONS[index].answers}
+      selectedAnswer={answer.selectedAnswer}
+      answerState={answerState}
+      onSelect={handleSelectAnswer}
+    />
+  </div>
+}
